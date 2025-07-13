4b3f911bca937a80576b6e56fca14bf46337f27e
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-14 00:32:22 +0600

    added GoogleStrategy by passport

---
+++ ph-tour-management-server/src/config/passport.ts
@@ -0,0 +1,60 @@
+import passport from "passport";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
+import { env_config } from ".";
+import { eAuthProvider, eUserRoles } from "../app/modules/user/user.interface";
+import { User } from "../app/modules/user/user.model";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: env_config.GOOGLE_CLIENT_ID,
+      clientSecret: env_config.GOOGLE_CLIENT_SECRET,
+      callbackURL: env_config.GOOGLE_CALLBACK_URL,
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        let message = "User already exist";
+        const email = profile.emails?.[0].value;
+        if (!email) {
+          message = "Email not found";
+          return done(null, false, { message });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          message = "User created successfully";
+
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            role: eUserRoles.USER,
+            isVerified: true,
+            auth: [
+              {
+                provider: eAuthProvider.google,
+                providerId: email,
+              },
+            ],
+          });
+        }
+
+        done(null, user, { message });
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log("❌ Google Strategy Error: ", error);
+        return done(error);
+      }
+    }
+  )
+);
