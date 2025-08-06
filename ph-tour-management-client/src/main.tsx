95486aad7f11ce06f6feec3b2774ad1c0e5c73ea
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-08-07 02:03:01 +0600

    updated

--- ph-tour-management-client/src/main.tsx
+++ ph-tour-management-client/src/main.tsx
@@ -1,13 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router";
 import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/ThemeProvider.tsx";
+import { store } from "./redux/store.ts";
 import { router } from "./routes/index.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <RouterProvider router={router} />
-    <Toaster />
-  </ThemeProvider>
+  <ReduxProvider store={store}>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
+  </ReduxProvider>
 );
