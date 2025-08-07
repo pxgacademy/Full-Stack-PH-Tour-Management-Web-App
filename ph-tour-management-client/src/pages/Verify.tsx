858675b8d1a272592047107c8cdd489d23874ff4
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-08-07 14:42:08 +0600

    added Verify component

--- ph-tour-management-client/src/pages/Verify.tsx
+++ ph-tour-management-client/src/pages/Verify.tsx
@@ -1,6 +1,18 @@
-//
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 export default function Verify() {
+  const navigate = useNavigate();
+  const { state: email } = useLocation();
+
+  useEffect(() => {
+    if (!email) {
+      navigate("/", { replace: true });
+    }
+  }, [email, navigate]);
+
+  if (!email) return null;
+
   return (
     <div className="">
       <h1 className="">This is Verify component</h1>
